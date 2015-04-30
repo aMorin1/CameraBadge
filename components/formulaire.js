6d078e74259aa6465b53abee7db8902e1cf46722
aMorin1 <alan.morin71@gmail.com>
2015-04-30 09:13:52 +0200

    Affichage de l'image convertie en base64

--- components/formulaire.js
+++ components/formulaire.js
@@ -2,20 +2,36 @@
 (function () {
 
   //Initialisation du module AngularJS et du module xeditable.
-  var app = angular.module('CamApp', []);
+  var app = angular.module('CamApp', ['ngTable']);
 
   //Controleur principal de l'app AngularJS
-  app.controller('CamController', function ($scope, $http) {
-    //$scope.tableEmp = false;
-    // Déclaration de l'URL du Serveur REST
-    var url = 'http://localhost/API_Rest/back/user';
+  app.controller('CamController', function ($scope, $http, ngTableParams) {
+    var url = 'http://localhost/API_Rest/back/user';                 
     
-    //Méthode GET -> Récupérer et afficher les données
-      $http.get(url)
-        .success(function(resultat) {
-          $scope.users = resultat;
-      });
-
+    $scope.users = [];
+    
+    // Déclaration de l'URL du Serveur REST
+    $scope.affichageTable = function() {
+        $http.get(url).success(function(resultat) {$scope.users = resultat});
+        setTimeout($scope.affichage, 1000);
+    };
+    $scope.affichageTable();
+    //Méthode GET -> Récupérer et afficher les
+      $scope.affichage = function() {
+        if($scope.users.length !== 0) {
+          $scope.tableParams = new ngTableParams({
+            page: 1,            
+            count: 3          
+            }, {
+                total: $scope.users.length,
+                getData: function($defer, params) {
+                    alert($scope.users.length);
+                    $defer.resolve($scope.users.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                }
+                });
+        };
+      };
+      $scope.affichage();
 });
 
 })();
